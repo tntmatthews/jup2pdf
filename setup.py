4144a2fc6f50b7a18e1cf7b587eab29dcacdd018
Michael Todd Matthews <mtoddmatthews@gmail.com>
2015-12-05 22:36:41 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='jup2pdf',
+    version='0.1',
+    description='Utility for Creating Jupyter Notebook Documents',
+    url='http://fl-gitlab/mmatthews/jup2pdf',
+    author='Michael Todd Matthews',
+    author_email='mmatthews@areva.com',
+    license='BSD',
+    packages=['jup2pdf'],
+    package_data={'latex': ['latex/*'],
+        'latex/areva': ['latex/areva/*'],'latex/jupyter': ['latex/jupyter/*']},
+    zip_safe=False)
